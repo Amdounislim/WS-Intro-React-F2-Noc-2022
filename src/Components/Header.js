6549bcceab0b667288d2c15dad75a79f19bf5863
Amdounislim <amdouni.slim@gmail.com>
2022-02-01 13:01:44 +0100

    first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+    let name = 'NavBar'
+    return (<div>
+        <h1>{name}</h1>
+        <ul>
+            <li>Home</li>
+            <li>Contact</li>
+            <li>Services</li>
+        </ul>
+    </div>
+    )
+        ;
+};
+
+export default Header;
+
